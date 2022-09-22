97ac033e4a705fb708daf0f2670d53d14dcc0a7d
zug <dungnguyen.ttcb@gmail.com>
2022-09-22 23:37:47 +0700

    Complete challenge for prj 2

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -16,11 +16,14 @@ class ViewController: UIViewController {
     var countries: [String] = []
     var score: Int = 0
     var correctAnswer: Int = 0
+    var answerTimes: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        answerTimes = 0
         
         button1.layer.borderWidth = 1
         button2.layer.borderWidth = 1
@@ -34,10 +37,15 @@ class ViewController: UIViewController {
     }
 
     func askQuestion(action: UIAlertAction! = nil) {
+        
+        if answerTimes == 10 {
+            score = 0
+            answerTimes = 0
+        }
+        
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
-        title = countries[correctAnswer].uppercased()
-        
+        title = countries[correctAnswer].uppercased() + "      Score: \(String(score))"
         
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
@@ -47,17 +55,32 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         var title: String
+        var mess: String
+        
+        answerTimes += 1
+        
 
         if sender.tag == correctAnswer {
             title = "Correct"
             score += 1
+            mess = "Your score is \(score)."
         } else {
             title = "Wrong"
             score -= 1
+            mess = "The correct answer is the picture number \(String(correctAnswer + 1))"
         }
         
-        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        if answerTimes == 10 {
+            title = "congratulations".uppercased()
+        }
+        
+        let ac = UIAlertController(title: title, message: mess, preferredStyle: .alert)
+        
+        if answerTimes == 10 {
+            ac.addAction(UIAlertAction(title: "Play again", style: .default, handler: askQuestion))
+        } else {
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        }
         present(ac, animated: true)
     }
 }
